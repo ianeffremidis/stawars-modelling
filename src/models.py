9fd28f8ede1147cce8e1f0b0a83c0e7c26ce79c3
ianeffremidis <ianeffremidis@gmail.com>
2022-11-28 20:45:58 +0000

    changes

--- src/models.py
+++ src/models.py
@@ -8,23 +8,66 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    password = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250))
+    gravity = Column(String(250))
+    url = Column(String(250))
+    
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(Integer)
+    homeworld = Column(String(250))
+    url = Column(String(250))
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250))
+    costInCredits = Column(String(250))
+    url = Column(String(250))
+
+
+class FavPlanets(Base):
+    __tablename__ = 'favPlanets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    userId = Column(Integer, ForeignKey('user.id'))
+    planetId = Column(Integer, ForeignKey('planet.id'))
+
+class FavCharacters(Base):
+    __tablename__ = 'favCharacters'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    userId = Column(Integer, ForeignKey('user.id'))
+    characterId = Column(Integer, ForeignKey('character.id'))
+
+class FavStarships(Base):
+    __tablename__ = 'favStarships'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    userId = Column(Integer, ForeignKey('user.id'))
+    starshipId =Column(Integer, ForeignKey('starship.id'))
 
     def to_dict(self):
         return {}
